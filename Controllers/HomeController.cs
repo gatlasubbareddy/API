4e36e680663941a5a805aa92de86660a9df8e155
gatlasubbareddy <58800789+gatlasubbareddy@users.noreply.github.com>
2020-09-16 13:11:04 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+     
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello World", "Welcome to Home page" };
+        }
+    }
+}
